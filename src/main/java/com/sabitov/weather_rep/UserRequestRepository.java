9a2a724a0173d4a5f6fb766e47ef439e4444221d
DDD <Sabitov.daniyar18@mail.ru>
2022-03-09 21:39:40 +0300

    add sign in, sign up services

--- src/main/java/com/sabitov/weather_rep/UserRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sabitov.weather_rep;
-
-import com.sabitov.models.UserRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRequestRepository extends JpaRepository<UserRequest, Long> {
-}
